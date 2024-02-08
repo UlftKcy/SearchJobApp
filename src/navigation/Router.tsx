0ca78d63f860384aa3e7cca46bccaea1346dd5c1
ulfet <ukacay@albasoft.com.tr>
2024-02-08 17:13:23 +0300

    added drawer route

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,16 +1,29 @@
-import Home from "@/screens/home";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Jobs from "@/screens/jobs";
+import FavoriteJobs from "@/screens/favorite-jobs";
 
-type RootStackParamList = {
-  Home: undefined;
-};
 
-const RootStack = createStackNavigator<RootStackParamList>();
+const RootStack = createStackNavigator();
+const RootDrawer = createDrawerNavigator();
+
+function Drawer() {
+  return (
+    <RootDrawer.Navigator initialRouteName="Jobs">
+      <RootDrawer.Screen name="Jobs" component={Jobs} />
+      <RootDrawer.Screen name="FavoriteJobs" component={FavoriteJobs} />
+    </RootDrawer.Navigator>
+  );
+}
 
 export default function Router() {
   return (
     <RootStack.Navigator>
-      <RootStack.Screen name="Home" component={Home} />
+      <RootStack.Screen
+        name="Drawer"
+        component={Drawer}
+        options={{ headerShown: false }}
+      />
     </RootStack.Navigator>
   );
 }
---
+++ src/screens/favorite-jobs/index.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function FavoriteJobs() {
+    return <View><Text>FavoriteJobs</Text></View>
+}
